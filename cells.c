12f0cccfcee2c963c345801fbe48c6d517ec48fc
David Zmick <dpzmick@gmail.com>
2013-05-31 17:13:22 -0500

    Revert "Revert "write in parallel with tasks""
    This reverts commit dbd90695aaff0d88b44c4e741168ba9bb226e72f.

--- cells.c
+++ cells.c
@@ -109,22 +109,17 @@ int main(int argc, char **argv) {
     //printf("Generating output\n");
     char *output = (char*) malloc(length * sizeof(char*));
 
-    char **buffer = (char**) malloc(timesteps * sizeof(char*));
-    for (int t = 0; t < timesteps; t++) {
-        buffer[t] = (char*) calloc(length, sizeof(char*));
-    }
     //printf("Running simulation\n");
+    #pragma omp parallel
     for (int t = 0; t < timesteps; t++) {
-        for (int i = 0; i < length; i++) {
-            buffer[t][i] = data[i];
-        }
-        //fwrite(data, sizeof(char), length, fp);
+        #pragma omp task
+        fwrite(data, sizeof(char), length, fp);
+        #pragma omp task
         rule(rule_no, data, length, output);
+        #pragma omp taskwait
         memcpy(data, output, length * sizeof(char));
     }
-    for (int t = 0; t < timesteps; t++) {
-        fwrite(buffer[t], sizeof(char), length, fp);
-    }
+
     fflush(fp);
     fclose(fp);
     free(data);
